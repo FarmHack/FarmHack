f292db20a3f69c5f1d8c16c55dc06233a2f8ac8d
rjsteinert <rj@rjsteinert.com>
2013-06-30 16:46:55 -0400

    M

--- home/app/views/Comment.js
+++ home/app/views/Comment.js
@@ -9,11 +9,18 @@ $(function() {
     render: function () {
       //this.$el.append(_.template(this.template(), this.model.toJSON()))
       //$('.recent-activity').append(_.template(this.template(), this.model.toJSON()))
+
+      var template = $("#template-Comment").html()
+      var vars = {"subject":"yar"}
+      var $el = _.template(template, vars)
+      $('.recent-activity').append($el)
+      /*
       var template = $("#template-Comment").html()
       var model = this.model.toJSON()
       var vars = {subject: model.subject}
       var $el = _.template(template, vars)
       $('.recent-activity').append($el)
+      */
     }
   })
 
