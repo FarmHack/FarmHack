d45ea95a0cc02dec88736f512bf84c893c3641c0
rjsteinert <rj@rjsteinert.com>
2013-06-30 16:44:56 -0400

    M

--- home/app/views/Comment.js
+++ home/app/views/Comment.js
@@ -10,7 +10,8 @@ $(function() {
       //this.$el.append(_.template(this.template(), this.model.toJSON()))
       //$('.recent-activity').append(_.template(this.template(), this.model.toJSON()))
       var template = $("#template-Comment").html()
-      var vars = this.model.toJSON()
+      var model = this.model.toJSON()
+      var vars = {subject: model.subject}
       var $el = _.template(template, vars)
       $('.recent-activity').append($el)
     }
