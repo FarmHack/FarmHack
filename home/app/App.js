db3de183900b463fe9e7f0cba572c09ef8d63ef5
rjsteinert <rj@rjsteinert.com>
2013-06-30 00:25:50 -0400

    Splash animation for jumbotron anonymous

---
+++ home/app/App.js
@@ -0,0 +1,35 @@
+$(function() {
+
+  App = new (Backbone.View.extend({
+
+    Models: {},
+    Views: {},
+    Collections: {},
+
+    el: 'body',
+
+    render: function(){
+      $.getJSON('/api/user', function(user) {
+        if (user.uid == 0) {
+          $('#jumbotron').hide()
+          $('#jumbotron').html($('#jumbotron-anonymous').html())
+          $('#jumbotron .frame').hide()
+          $('#jumbotron .content').hide()
+          $('#jumbotron').show()
+          $('#jumbotron .frame').css('height', '210px')
+          $('#jumbotron .frame').delay(500).slideDown(700)
+          $('#jumbotron .content').delay(1500).fadeIn()
+        }
+        else {
+
+        }
+      })
+    },
+
+    start: function(){
+      this.render()
+    }
+
+  }))
+  
+})
