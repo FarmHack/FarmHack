ea9d6d891fa6f03055e36a6347812cd4fa7d5cbc
rjsteinert <rj@rjsteinert.com>
2013-06-30 17:10:40 -0400

    m

--- home/app/views/Comment.js
+++ home/app/views/Comment.js
@@ -3,8 +3,8 @@ $(function() {
   App.Views.Comment = Backbone.View.extend({
 
     render: function () {
-      this.$el.append(_.template($("#template-Comment").html(), this.model.toJSON()))
-      //$('.recent-activity').append(_.template(this.template(), this.model.toJSON()))
+      //this.$el.append(_.template($("#template-Comment").html(), this.model.toJSON()))
+      $('.recent-activity').append(_.template(this.template(), this.model.toJSON()))
 
       /*
       var template = $("#template-Comment").html()
