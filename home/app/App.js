6f3b27d7a7caf6a3395657d6ade685f77e8bff76
rjsteinert <rj@rjsteinert.com>
2013-06-30 00:43:16 -0400

    simpler app.js and hello returning user

--- home/app/App.js
+++ home/app/App.js
@@ -6,11 +6,10 @@ $(function() {
     Views: {},
     Collections: {},
 
-    el: 'body',
-
-    render: function(){
+    start: function(){
       $.getJSON('/api/user', function(user) {
         if (user.uid == 0) {
+          // @todo I could simplify this using transparency instead of tricky hides and css?
           $('#jumbotron').hide()
           $('#jumbotron').html($('#jumbotron-anonymous').html())
           $('#jumbotron .frame').hide()
@@ -21,13 +20,16 @@ $(function() {
           $('#jumbotron .content').delay(1200).fadeIn()
         }
         else {
-
+          $('#jumbotron').hide()
+          $('#jumbotron').html("<div class='frame'><h1 class='content'>Welcome back " + user.name + "</h1></div>")
+          $('#jumbotron .frame').hide()
+          $('#jumbotron .content').hide()
+          $('#jumbotron').show()
+          $('#jumbotron .frame').css('height', '80px')
+          $('#jumbotron .frame').delay(500).slideDown(700)
+          $('#jumbotron .content').delay(1200).fadeIn()
         }
       })
-    },
-
-    start: function(){
-      this.render()
     }
 
   }))
