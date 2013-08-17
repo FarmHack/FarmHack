47bfe28e30b720b4dbfb38f3446c2e393d9c6002
rjsteinert <rj@rjsteinert.com>
2013-08-17 16:32:19 -0400

    Background for top in home

--- home/app/App.js
+++ home/app/App.js
@@ -12,8 +12,8 @@ $(function() {
     },
 
     renderJumboTron: function () {
-      $.getJSON('/api/user', function(user) {
-        if (user.uid == 0) {
+      //$.getJSON('/api/user', function(user) {
+        //if (user.uid == 0) {
           // @todo I could simplify this using transparency instead of tricky hides and css?
           $('#jumbotron').hide()
           $('#jumbotron').html($('#jumbotron-anonymous').html())
@@ -23,12 +23,12 @@ $(function() {
           $('#jumbotron .frame').css('height', '210px')
           $('#jumbotron .frame').delay(500).slideDown(700)
           $('#jumbotron .content').delay(1200).fadeIn()
-        }
-        else {
-          $('#jumbotron').html("<h1 style='display:none; margin:0;' class='content'>Welcome back " + user.name + "</h1>")
-          $('#jumbotron .content').delay(500).fadeIn()
-        }
-      })
+       // }
+       // else {
+       //   $('#jumbotron').html("<h1 style='display:none; margin:0;' class='content'>Welcome back " + user.name + "</h1>")
+       //   $('#jumbotron .content').delay(500).fadeIn()
+       // }
+      //})
     },
 
     renderRecentActivity: function() {
