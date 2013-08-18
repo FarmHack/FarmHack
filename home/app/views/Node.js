4d4e4a1d700491209bbc325529479177ef832826
rjsteinert <rj@rjsteinert.com>
2013-08-18 01:03:01 -0400

    Way better info now displayed on comments and nodes

--- home/app/views/Node.js
+++ home/app/views/Node.js
@@ -7,6 +7,8 @@ $(function() {
     },
 
     render: function () {
+      var vars = this.model.toJSON()
+      vars.changed = moment(vars.changed, "X").format("MMM D h:mma")
       this.$el.append(_.template(this.template(), this.model.toJSON()))
     }
   })
