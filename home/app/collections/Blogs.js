3ff746db36eb1ef176ce682bcc07e56199814197
rjsteinert <rj@rjsteinert.com>
2014-08-09 16:48:57 -0400

    3 param for app as well

--- home/app/collections/Blogs.js
+++ home/app/collections/Blogs.js
@@ -2,7 +2,7 @@ $(function() {
 
   App.Collections.Blogs = Backbone.Collection.extend({
     //url: "http://dev.farmhack.gotpantheon.com/api/blogs",
-    url: "/api/blogs",
+    url: "/api/blogs/recent",
     model: App.Models.Blog
   })
 
