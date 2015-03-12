21b19440d70a7b78cbfe52803c9b5ef24d99a24e
R.J. Steinert <rj@rjsteinert.com>
2015-03-12 14:52:09 -0400

    #8 Remove cruft from prototype for minimal structures

--- home2/app/Router.js
+++ home2/app/Router.js
@@ -2,43 +2,11 @@ $(function(){
 
    App.Router = new (Backbone.Router.extend({
 
-
       routes:{
-        '': 'hacksRoute',
-        'hacks': 'hacksRoute',
-        'meet-ups': 'meetUpsRoute',
-        'tool/:toolId': 'tool'
+        '': 'home',
       },
+      home: function() {
 
-      tool: function(toolId) {
-        var tpl = _.template(jQuery('#template-tool-42').html())
-        $('#main-region').html(tpl())
-
-      },
-
-      hacksRoute: function() {
-        var tpl = _.template(jQuery('#template-hacks').html())
-        jQuery('#main-region').html(tpl())
-        jQuery('.search-form').one('click', function() {
-          jQuery('.search-form .advanced')  .css('opacity', 0)
-          .slideDown('slow')
-          .animate(
-            { opacity: 1 },
-            { queue: false, duration: 'slow' }
-          );
-        })
-        jQuery('.see-more').one('click', function() {
-          jQuery('.page2')  .css('opacity', 0)
-          .slideDown('slow')
-          .animate(
-            { opacity: 1 },
-            { queue: false, duration: 'slow' }
-          );
-        })
-      },
-
-      meetUpsRoute: function() {
-        jQuery('#main-region').html('meet ups')
       }
   }))
 
