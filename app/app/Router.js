f0c815beb98d5b9e7711ae6450e03b1df2a0ea93
R.J. Steinert <rj@rjsteinert.com>
2015-04-27 10:59:29 -0400

    Add views to home

--- app/app/Router.js
+++ app/app/Router.js
@@ -11,31 +11,34 @@ $(function(){
         // Declare View instances
         var toolCardsView = new App.Views.ToolCards()
         var allInOneBannerView = new App.Views.AllInOneBanner()
-        var farmHackMovieView = new App.Views.FarmHackMovie()
+        var statementView = new App.Views.Statement()
         var blogView = new App.Views.JumboBlog()
         var searchForm = new App.Views.SearchForm()
+        var navBarView = new App.Views.NavBar()
 
         // Give the Views data
         toolCardsView.collection = new App.Collections.FeaturedTools()
         blogView.collection = new App.Collections.Blogs()
 
         // Assign View instances to Regions
+        $('body').prepend(navBarView.el)
         $('.main-region').html('')
         $('.main-region').append(allInOneBannerView.el)
-        $('.main-region').append(farmHackMovieView.el)
+        $('.main-region').append(statementView.el)
         $('.main-region').append(blogView.el)
-        $('.main-region').append('<img src="images/hr.png" width=990 style="margin: 15px 0px 15px;">')
+        $('.main-region').append('<img class="hidden-sm hidden-xs" src="images/hr.png" width=990 style="margin: 15px 0px 15px;">')
         $('.main-region').append(searchForm.el)
         $('.main-region').append(toolCardsView.el)
 
         // Render
         allInOneBannerView.render()
-        farmHackMovieView.render()
+        statementView.render()
         blogView.render()
         toolCardsView.render()
         searchForm.render()
+        navBarView.render()
 
-      }
+      },
 
   }))
 
