e8ecc2ad6f0f3f6f60861ba67995c5e68d90343e
R.J. Steinert <rj@rjsteinert.com>
2015-04-30 11:44:42 -0400

    Add placeholder image for FarmHackMovie View in app

--- app/app/views/FarmHackMovie/FarmHackMovie.js
+++ app/app/views/FarmHackMovie/FarmHackMovie.js
@@ -5,12 +5,16 @@ $(function() {
 
     className: 'farm-hack-movie pull-left',
 
-    template: _.template(' \
+    movieTemplate: _.template(' \
         <iframe src="//player.vimeo.com/video/73504647?portrait=0" width="600" height="337" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe> \
     '),
 
+    placeholderTemplate: _.template(' \
+        <img src="app/views/FarmHackMovie/farm-hack-movie.png" width="600" height="337" /> \
+    '),
+
     render: function () {
-      this.$el.append(this.template())
+      this.$el.append(this.placeholderTemplate())
     }
 
   })
