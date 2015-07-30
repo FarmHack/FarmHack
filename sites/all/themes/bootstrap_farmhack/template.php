986524fef4946e555bd3850a88a83d67b13c81a7
R.J. Steinert <rj@rjsteinert.com>
2015-07-30 10:33:30 -0400

    #25 Tweak the loading process of tools search and change the url to /library/tools

--- sites/all/themes/bootstrap_farmhack/template.php
+++ sites/all/themes/bootstrap_farmhack/template.php
@@ -4,20 +4,27 @@
  * template.php
  */
 function bootstrap_farmhack_apachesolr_search_page_alter(array &$build, array $search_page) {
-  if (arg(0) == "library" && arg(1) == "search") {
-    // Horrible ugly hacks because so many things are broken in Drupal
-    drupal_add_js( 'setTimeout(function() { 
-      jQuery(".search-results").packery({ itemSelector: ".item",  gutter: 10 }) 
-      jQuery(".page-header").text("Tool Library")
-      jQuery(\'#block-system-main h2\').hide()
-      }, 100)
+  if (arg(0) == "library" && arg(1) == "tools") {
+    // Manage the loading of the library/tools page
+    drupal_add_js( '
+      jQuery(window).load(function() {
+        jQuery(".spinner").hide()
+        jQuery(".search-results").fadeIn()
+        jQuery(".search-results").packery({ itemSelector: ".item",  gutter: 10 }) 
+      })
+      document.addEventListener("DOMContentLoaded", function(event) { 
+        jQuery(".page-header").text("Tool Library")
+        jQuery(\'#block-system-main h2\').hide()
+        jQuery(".search-results").hide()
+        jQuery(".search-results").before("<center><img class=\"spinner\" src=\"/sites/all/themes/bootstrap_farmhack/images/spinner.gif\" align=\"center\"></center>")
+      })
     ', 'inline');
   }
 }
 
 
 function bootstrap_farmhack_preprocess_search_result(&$vars) {
-  if (arg(0) == "library" && arg(1) == "search") {
+  if (arg(0) == "library" && arg(1) == "tools") {
     $vars['nid'] = $vars['result']['node']->entity_id;
     $vars['url'] = drupal_get_path_alias('node/' . $vars['nid']);
     $vars['node'] = node_load($vars['result']['node']->entity_id);
